41927a81aedbb5135c6b50db00e15dbec85cc78f
Drew <158089597+dcdolot@users.noreply.github.com>
2024-06-21 16:19:45 +0800

    Make a getHumanChoice function to get user pick

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ function getComputerChoice() {
 
 computerChoice = getComputerChoice()
 
+function getHumanChoice() {
+    let humanChoice = window.prompt("Please input rock, paper, or scissors")
+    humanChoice = humanChoice.toLowerCase()
+
+    if(humanChoice == "rock") {
+        return "rock";
+    }
+    else if(humanChoice == "paper") {
+        return "paper";
+    }
+    else if(humanChoice == "scissors") {
+        return "scissors";
+    }
+    else {
+        console.log("Invalid choice")
+        humanChoice = getHumanChoice()
+    }
+
+
+    return humanChoice
+}
+
+humanChoice = getHumanChoice()
+
+
