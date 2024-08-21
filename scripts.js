65d1c1388be1bf74ca2036fcff9e1d217f7a3a1f
Drew <158089597+dcdolot@users.noreply.github.com>
2024-08-21 23:28:12 +0800

    Add div for displaying results and change console.log into DOM methods

--- scripts.js
+++ scripts.js
@@ -7,34 +7,46 @@ function playRound(playerSelection) {
     const choices = ["Rock", "Paper", "Scissors"];
     const computerSelection = choices[Math.floor(Math.random() * choices.length)];
 
-    console.log(`Player: ${playerSelection}, Computer: ${computerSelection}`);
+    const selectionDiv = document.getElementById("selection")
+
+    selectionDiv.textContent = (`Player: ${playerSelection}, Computer: ${computerSelection}`);
+
+    const resultPerRound = document.getElementById("results")
 
     if (playerSelection === computerSelection) {
-        // console.log("It's a tie!");
-        return "It's a tie!";
+
+
+        resultPerRound.textContent = "It's a tie!";
+        // return "It's a tie!";
     } else if (
         (playerSelection === "Rock" && computerSelection === "Scissors") ||
         (playerSelection === "Paper" && computerSelection === "Rock") ||
         (playerSelection === "Scissors" && computerSelection === "Paper")
     ) {
         playerScore++;
-        // console.log("You win this round!");
-        return "You win this round!";
+        resultPerRound.textContent = "You win this round!";
+        // return "You win this round!";
     } else {
         computerScore++;
-        // console.log("Computer wins this round!");
-        return "Computer wins this round!";
+        resultPerRound.textContent = "Computer wins this round!";
+        // return "Computer wins this round!";
     }
 }
 
 function checkWinner() {
-    console.log(`Final Scores - Player: ${playerScore}, Computer: ${computerScore}`);
+    const overallPointsResult = document.getElementById("overallPoints");
+    
+    overallPointsResult.textContent = (`Final Scores - Player: ${playerScore}, Computer: ${computerScore}`);
+
+    const overallGameWinner = document.getElementById("overallWinner");
+    
     if (playerScore > computerScore) {
-        console.log("You are the overall winner!");
+
+        overallGameWinner.textContent = "You are the overall winner!";
     } else if (playerScore < computerScore) {
-        console.log("Computer is the overall winner!");
+        overallGameWinner.textContent = "Computer is the overall winner!";
     } else {
-        console.log("The game is a tie!");
+        overallGameWinner.textContent = "The game is a tie!";
     }
 }
 
